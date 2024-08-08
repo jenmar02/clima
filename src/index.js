44c20f55682433c9c4bd2cf8ee71d5181a8522f7
jenmar02 <156864399+jenmar02@users.noreply.github.com>
2024-08-07 22:48:13 -0500

    added search functionalities

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let searchTemp = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-city");
+
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(searchTemp);
+}
+function searchCity(city) {
+  let apiKey = "e49t4e467d35faocb8ec3a1644a604fa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
 
-  let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = searchInput.value;
-
-  //call API
-  //search for the city
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Austin");
